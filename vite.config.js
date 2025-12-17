3d184fb406ba8ed3e3785a5eb62c9020058deda5
UmerDesigns0-0 <umerandlife@gmail.com>
2025-12-17 18:06:46 +0500

    Uncomment Vite configuration for React and Tailwind CSS

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
+// import { defineConfig } from "vite";
+// import tailwindcss from "@tailwindcss/vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+// });
+
+// vite.config.js
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
